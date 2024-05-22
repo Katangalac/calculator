c144bec536c221d4cdb475b9a520a38d6d6d01f3
Katangalac <katangalachristian@yahoo.com>
2024-05-22 15:45:11 -0400

    Add basics operations, trigonometric functions, and converters for number and unit. Also add the main window of the GUI

---
+++ Calculator/src/main/java/Calculator/Main.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator.Gui.MainWindow mainWindow = new Calculator.Gui.MainWindow();
+        mainWindow.setVisible(true);
+    }
+}
